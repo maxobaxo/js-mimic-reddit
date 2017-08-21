d81faa9cd0f8812768299056083b51266fd669f9
Max Scher <maxobaxo@gmail.com>
2017-08-21 16:08:39 -0700

    add mock posts with new category property

--- src/app/post.model.ts
+++ src/app/post.model.ts
@@ -1,3 +1,3 @@
 export class Post {
-  constructor (public title: string, public author: string, public content: string, public id: number) {}
+  constructor (public title: string, public author: string, public content: string, public category: string, public id: number) {}
 }
